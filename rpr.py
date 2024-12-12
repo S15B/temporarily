6cd7098f666889aed7b4b4a49b200ddcf891b6a5
S15B <134049563+S15B@users.noreply.github.com>
2024-12-13 01:55:58 +0300

    Update rpr.py

--- rpr.py
+++ rpr.py
@@ -13,7 +13,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 # Создание плоскости
 p.loadURDF("plane.urdf")
 
-radius = 0.05
+radius = 0.07
 height = 0.05
 length_leg = 0.5
 
@@ -28,31 +28,30 @@ leg_visual_shape_2 = p.createVisualShape(p.GEOM_BOX, halfExtents=[0.05, 0.03, le
 leg_collision_shape_2 = p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.05, 0.03, length_leg/2])
 
 # Создание первого фиктивного неподвижного тела
-anchor_position_1 = [0, 0, 1]
 anchor_id_1 = p.createMultiBody(
     baseMass=0,                         
-    basePosition=anchor_position_1,      
+    basePosition=[0, 0, 1],      
     baseCollisionShapeIndex=-1,          
     baseVisualShapeIndex=p.createVisualShape(p.GEOM_CYLINDER, radius=radius, length=height),
-    linkMasses=[1, 1],
-    linkCollisionShapeIndices=[leg_collision_shape_1, sphere_collision_shape],
-    linkVisualShapeIndices=[leg_visual_shape_1, sphere_visual_shape],
-    linkPositions=[[0, 0, 0], [0, 0, np.sqrt(2)/2]],
-    linkOrientations=[p.getQuaternionFromEuler([0, np.pi/4, 0]), p.getQuaternionFromEuler([0, 0, 0])],
-    linkInertialFramePositions=[[0, 0, 0], [0, 0, 0]],
-    linkInertialFrameOrientations=[p.getQuaternionFromEuler([0, 0, 0]), p.getQuaternionFromEuler([0, 0, 0])],
-    linkParentIndices=[0, 1],
-    linkJointTypes=[p.JOINT_REVOLUTE, p.JOINT_PRISMATIC],
-    linkJointAxis=[[0, -1, 0], [0, 0, 1]] 
+    linkMasses=[1],
+    linkCollisionShapeIndices=[leg_collision_shape_1],
+    linkVisualShapeIndices=[leg_visual_shape_1],
+    linkPositions=[[0, 0, 0]],
+    linkOrientations=[p.getQuaternionFromEuler([0, np.pi/4, 0])],
+    linkInertialFramePositions=[[0, 0, 0]],
+    linkInertialFrameOrientations=[p.getQuaternionFromEuler([0, 0, 0])],
+    linkParentIndices=[0],
+    linkJointTypes=[p.JOINT_REVOLUTE],
+    linkJointAxis=[[0, 1, 0]] 
 )
 
 # Создание второго фиктивного неподвижного тела
-anchor_position_2 = [1, 0, 2]
 anchor_id_2 = p.createMultiBody(
     baseMass=0,                          
-    basePosition=anchor_position_2,      
+    basePosition=[1, 0, 2],      
     baseCollisionShapeIndex=-1,          
     baseVisualShapeIndex=p.createVisualShape(p.GEOM_CYLINDER, radius=radius, length=height),
+    baseOrientation=p.getQuaternionFromEuler([0, 0, 0]),
     linkMasses=[1],
     linkCollisionShapeIndices=[leg_collision_shape_2],
     linkVisualShapeIndices=[leg_visual_shape_2],
@@ -65,31 +64,70 @@ anchor_id_2 = p.createMultiBody(
     linkJointAxis=[[0, 1, 0]] 
 )
 
+# Создание платформы
+platform_id = p.createMultiBody(
+    baseMass=1,
+    baseCollisionShapeIndex=p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.05, 0.03, length_leg/2]),
+    baseVisualShapeIndex=p.createVisualShape(p.GEOM_BOX, halfExtents=[0.05, 0.03, length_leg/2]),
+    basePosition=[0.5, 0, 1.5],
+    baseOrientation=p.getQuaternionFromEuler([0, np.pi/4, 0]),
+    linkMasses=[1, 1],
+    linkCollisionShapeIndices=[p.createCollisionShape(p.GEOM_CYLINDER, radius=radius, height=height),
+                               p.createCollisionShape(p.GEOM_CYLINDER, radius=radius, height=height)],
+    linkVisualShapeIndices=[p.createVisualShape(p.GEOM_CYLINDER, radius=radius, length=height),
+                            p.createVisualShape(p.GEOM_CYLINDER, radius=radius, length=height)],
+    linkPositions=[[0, 0, -length_leg/2], [0, 0, length_leg/2]],
+    linkOrientations=[p.getQuaternionFromEuler([0, 0, 0]), p.getQuaternionFromEuler([0, 0, 0])],
+    linkInertialFramePositions=[[0, 0, 0], [0, 0, 0]],
+    linkInertialFrameOrientations=[p.getQuaternionFromEuler([0, 0, 0]), p.getQuaternionFromEuler([0, 0, 0])],
+    linkParentIndices=[0, 0],
+    linkJointTypes=[p.JOINT_REVOLUTE, p.JOINT_REVOLUTE],
+    linkJointAxis=[[0, 1, 0], [0, 1, 0]]
+)
+
 # Создание ограничения (шарнир между фиктивным телом и платформой)
-constraint_id = p.createConstraint(
-    parentBodyUniqueId=anchor_id_2, 
+constraint_id_1 = p.createConstraint(
+    parentBodyUniqueId=anchor_id_1, 
     parentLinkIndex=0,
-    childBodyUniqueId=anchor_id_1,
-    childLinkIndex=1,
+    childBodyUniqueId=platform_id,
+    childLinkIndex=0,
+    childFrameOrientation=p.getQuaternionFromEuler([0, 0, 0]),
     jointType=p.JOINT_PRISMATIC, 
     jointAxis=[0, 0, 1],  # Движение вдоль оси Z
     parentFramePosition=[0, 0, 0],  
     childFramePosition=[0, 0, 0]  
 )
+p.changeDynamics(constraint_id_1, 1, linearDamping=0, angularDamping=0, contactDamping=0, contactStiffness=0)
+
+# Создание ограничения (шарнир между фиктивным телом и платформой)
+constraint_id_2 = p.createConstraint(
+    parentBodyUniqueId=anchor_id_2, 
+    parentLinkIndex=0,
+    childBodyUniqueId=platform_id,
+    childLinkIndex=1,
+    childFrameOrientation=p.getQuaternionFromEuler([0, 0, 0]),
+    jointType=p.JOINT_PRISMATIC, 
+    jointAxis=[0, 0, 1],  # Движение вдоль оси Z
+    parentFramePosition=[0, 0, 0],  
+    childFramePosition=[0, 0, 0]  
+)
+p.changeDynamics(constraint_id_2, 1, linearDamping=0, angularDamping=0, contactDamping=0, contactStiffness=0)
 
 p.changeDynamics(anchor_id_1, 0, linearDamping=0, angularDamping=0)
 p.changeDynamics(anchor_id_2, 0, linearDamping=0, angularDamping=0)
-p.changeDynamics(anchor_id_1, 1, linearDamping=0, angularDamping=0)
-p.changeDynamics(constraint_id, 1, linearDamping=0, angularDamping=0)
+p.changeDynamics(platform_id, 0, linearDamping=0, angularDamping=0)
+p.changeDynamics(platform_id, 1, linearDamping=0, angularDamping=0)
+
 
 for i in range(10000):
+   
     p.setJointMotorControl2(anchor_id_1, 0, controlMode=p.VELOCITY_CONTROL, targetVelocity=-1, force=10)
-    p.setJointMotorControl2(anchor_id_2, 0, controlMode=p.VELOCITY_CONTROL, targetVelocity=-1, force=10)
-    p.setJointMotorControl2(anchor_id_1, 1, controlMode=p.POSITION_CONTROL, targetPosition=1/2 - length_leg/2, force=10)
-    p.setJointMotorControl2(constraint_id, 1, controlMode=p.POSITION_CONTROL, targetPosition=-1/2 + length_leg/2, force=1)
+    p.setJointMotorControl2(anchor_id_2, 0, controlMode=p.VELOCITY_CONTROL, targetVelocity=1, force=10)
+    #p.setJointMotorControl2(constraint_id_1, 0, controlMode=p.VELOCITY_CONTROL, targetVelocity=0.01, force=10)
+    #p.setJointMotorControl2(constraint_id_2, 0, controlMode=p.VELOCITY_CONTROL, targetVelocity=-0.01, force=10)
     
     p.stepSimulation()
     time.sleep(1 / 240)
 
 # Завершение симуляции
-p.disconnect
+# p.disconnect
